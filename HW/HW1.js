c4773a4a868c26df6ae89e5d56dd5882b73db367
SonicNiJiaJia <151108426+SonicNiJiaJia@users.noreply.github.com>
2024-10-28 16:46:24 +0800

    hi

---
+++ HW/HW1.js
@@ -0,0 +1,6 @@
+var age = 25;
+var Name = "Alice";
+age = 30;
+Name = "Sonic";
+console.log(age);
+console.log(Name);
---
+++ HW/HW1.ts
@@ -0,0 +1,9 @@
+const age: number = 25; 
+let Name: string = "Alice"; 
+
+age = 30; // Cannot assign to 'age' because it is a constant.
+
+Name = "Sonic"; 
+
+console.log(age);
+console.log(Name);
